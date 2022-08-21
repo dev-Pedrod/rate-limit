bbd4b7490159e6323436b528b22b02dcf138d556
João Pedro <pedroolivxira@gmail.com>
2022-08-21 09:41:56 -0300

    User entity

---
+++ src/main/java/com/devpedrod/ratelimit/domain/User.java
@@ -0,0 +1,21 @@
+package com.devpedrod.ratelimit.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity(name = "_user")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+}
