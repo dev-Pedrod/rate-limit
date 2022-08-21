d182638db52005a08f77cc329b4916c46e06f35d
João Pedro <pedroolivxira@gmail.com>
2022-08-21 11:26:40 -0300

    Implements user details service

--- src/main/java/com/devpedrod/ratelimit/service/UserService.java
+++ src/main/java/com/devpedrod/ratelimit/service/UserService.java
@@ -4,21 +4,34 @@ import com.devpedrod.ratelimit.domain.User;
 import com.devpedrod.ratelimit.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class UserService {
+public class UserService implements UserDetailsService{
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public void create(User user){
         log.info("New user created, id: {}", user.getUsername());
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
     public User getById(Long id) {
         log.info("Find user by id {}", id);
         return userRepository.findById(id).get();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByUsername(username)
+                .orElseThrow(() ->  new UsernameNotFoundException(String.format("user with username %s not found", username)));
     }
 }
