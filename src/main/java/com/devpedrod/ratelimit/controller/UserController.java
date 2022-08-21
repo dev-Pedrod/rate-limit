f0bdb95203cd856a54d1ef6eea6135ca8e0f66b7
João Pedro <pedroolivxira@gmail.com>
2022-08-21 11:27:20 -0300

    Set rate limit to API

--- src/main/java/com/devpedrod/ratelimit/controller/UserController.java
+++ src/main/java/com/devpedrod/ratelimit/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.devpedrod.ratelimit.controller;
 
 import com.devpedrod.ratelimit.domain.User;
+import com.devpedrod.ratelimit.service.RateLimitService;
 import com.devpedrod.ratelimit.service.UserService;
+import io.github.bucket4j.ConsumptionProbe;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -13,6 +18,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private RateLimitService rateLimit;
 
     @PostMapping()
     public ResponseEntity<Void> createUser(@RequestBody User user){
@@ -26,6 +33,17 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getById(@PathVariable Long id){
-        return ResponseEntity.ok(userService.getById(id));
+        ConsumptionProbe probe = rateLimit.getBucket().tryConsumeAndReturnRemaining(1);
+        if (probe.isConsumed()){
+            var user = userService.getById(id);
+            return ResponseEntity.ok()
+                    .header("X-Rate-Limit-Remaining", String.valueOf(probe.getRemainingTokens()))
+                    .body(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                    .header("X-Rate-Limit-Retry-After-Seconds",
+                            String.valueOf(TimeUnit.NANOSECONDS.toSeconds(probe.getNanosToWaitForRefill())))
+                    .build();
+        }
     }
 }
